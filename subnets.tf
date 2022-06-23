366ec16286daeb8c8606bb02859ba2a4d3ba842a
Murshed J. Ahmed <mahmed@marvel.com>
2022-06-23 16:58:13 -0400

    Create Subnets

---
+++ subnets.tf
@@ -0,0 +1,65 @@
+resource "aws_subnet" "public_01" {
+  vpc_id = aws_vpc.main.id
+
+  # Portion of VPC CIDR allocated to this subnet
+  cidr_block = "192.168.0.0/18"
+
+  # TODO: Store in a variable
+  availability_zone = "us-east-1a"
+
+  # EKS Requirement
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "EKS Public Subnet 01"
+
+    # Allow cluster to discover this subnet
+    "kubernetes.io/cluster/eks" = "shared"
+
+    # Allows public load balancer created by EKS to be placed in this subnet
+    "kubernetes.io/role/elb" = 1
+    terraform                = "true"
+  }
+}
+
+resource "aws_subnet" "public_02" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "192.168.64.0/18"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                        = "EKS Public Subnet 02"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+    terraform                   = "true"
+  }
+}
+
+resource "aws_subnet" "private_01" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "192.168.128.0/18"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name                        = "EKS Private Subnet 01"
+    "kubernetes.io/cluster/eks" = "shared"
+
+    # Deploy private load balancer
+    "kubernetes.io/role/internal-elb" = 1
+    terraform                         = "true"
+  }
+}
+
+resource "aws_subnet" "private_02" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "192.168.192.0/18"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name                              = "EKS Private Subnet 02"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+    terraform                         = "true"
+  }
+}
